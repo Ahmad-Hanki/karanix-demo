afc021a300469c6ee2ce35bf142833c5cc487606
Ahmad <itxti909@gmail.com>
2025-11-20 20:13:51 +0300

    feat: nextjs initlize

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -1,10 +1,12 @@
 type Configuration = {
   PORT: number;
   NODE_ENV: 'development' | 'production';
+  JWT_SECRET: string;
 };
 
 export default (): Configuration => ({
   PORT: Number(process.env.PORT) || 8000,
   NODE_ENV:
     (process.env.NODE_ENV as 'development' | 'production') || 'development',
+  JWT_SECRET: process.env.JWT_SECRET || 'defaultsecret',
 });
